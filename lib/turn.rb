07914a454804cdf87c38723b68bacbd399e722de
Justin Phillips <justinphil02@yahoo.com>
2017-08-31 02:03:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,11 +16,11 @@ def turn
 end
 
 def display_board(array)
-puts " #{array[0]} | #{array[1]} | #{array[2]}"
+puts " #{array[0]} | #{array[1]} | #{array[2]} "
 puts "-----------"
-puts " #{array[3]} | #{array[4]} | #{array[5]}"
+puts " #{array[3]} | #{array[4]} | #{array[5]} "
 puts "-----------"
-puts " #{array[6]} | #{array[7]} | #{array[8]}"
+puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
 
 def valid_move?(array, index)
